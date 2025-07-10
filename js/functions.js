a438e443c4ef4b1ff1b703ec2a374d1f2763f1ce
Михаил <140623282+MishaDerbilov@users.noreply.github.com>
2025-07-10 14:49:14 +0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,6 @@ function isStringLengthValid(str, maxLength) {
   return str.length <= maxLength;
 }
 
-console.log(isStringLengthValid('проверяемая строка', 20));
-console.log(isStringLengthValid('проверяемая строка', 18)); 
-console.log(isStringLengthValid('проверяемая строка', 10)); 
-
 function isPalindrome(str) {
   const normalized = str.replaceAll(' ', '').toLowerCase();
   let reversed = '';
@@ -16,8 +12,3 @@ function isPalindrome(str) {
 
   return normalized === reversed;
 }
-
-console.log(isPalindrome('топот'));                       
-console.log(isPalindrome('ДовОд'));                       
-console.log(isPalindrome('Кекс'));                        
-console.log(isPalindrome('Лёша на полке клопа нашёл '));  
