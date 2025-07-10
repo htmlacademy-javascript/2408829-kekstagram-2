7023ec034202fbe66c3d66326fdc7c196fd4a22e
Keks <keks@htmlacademy.ru>
2025-07-10 13:59:16 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,15 @@
+function isStringLengthValid(str, maxLength) {
+  return str.length <= maxLength;
+}
+function isPalindrome(str) {
+  const normalized = str.replaceAll(' ', '').toLowerCase();
+  let reversed = '';
+  for (let i = normalized.length - 1; i >= 0; i--) {
+    reversed += normalized[i];
+  }
+  return normalized === reversed;
+}
+console.log(isStringLengthValid('проверяемая строка', 20));
+console.log(isStringLengthValid('проверяемая строка', 20));
+console.log(isPalindrome('топот')); 
+console.log(isPalindrome('Кекс')); 
