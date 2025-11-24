76eb4435374eaaff0e36a6373d75184e6327ac35
Михаил <140623282+MishaDerbilov@users.noreply.github.com>
2025-11-24 22:25:27 +0400

    1

--- js/main.js
+++ js/main.js
@@ -3,19 +3,27 @@ import { renderThumbnails } from './render-thumbnails.js';
 import { openFullsizePhoto } from './render-fullsize.js';
 import './form.js';
 
-const photos = generatePhotoDescriptions();
-renderThumbnails(photos);
+getData()
+  .then((photos) => {
+    renderThumbnails(photos);
 
-const picturesContainer = document.querySelector('.pictures');
+    const picturesContainer = document.querySelector('.pictures');
 
-picturesContainer.addEventListener('click', (evt) => {
-  const picture = evt.target.closest('.picture');
-  if (!picture) {
-    return;
-  }
+    picturesContainer.addEventListener('click', (evt) => {
+      const picture = evt.target.closest('.picture');
+      if (!picture) {
+        return;
+      }
 
-  const index = picture.dataset.index;
-  if (index !== undefined) {
-    openFullsizePhoto(photos[+index]);
-  }
-});
+      const index = picture.dataset.index;
+      if (index !== undefined) {
+        openFullsizePhoto(photos[+index]);
+      }
+    });
+  })
+  .catch(() => {
+    const errorTemplate = document
+      .querySelector('#data-error')
+      .content.querySelector('.data-error');
+    document.body.append(errorTemplate.cloneNode(true));
+  });
